56fce8ca4cabe836f39def0d6232e653425b877a
Dipesh Kumar <dipeshkumar@microsoft.com>
2022-05-26 16:38:23 +0530

    Fixed the error during merge

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Markup/Baml2006/Baml2006Reader.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Markup/Baml2006/Baml2006Reader.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xaml;
+using System.Buffers;
 using System.Diagnostics;
 using System.ComponentModel;
 using System.Reflection;
